6b057f016ba6a496197359e3fc573bd61401ecde
Beatriz Pinheiro <beatriz.pinheiro@mobrj.com.br>
2022-05-11 13:42:19 -0300

    Primeira versão da Página Inicial pós Login

--- NubankClone/NubankClone/App.xaml.cs
+++ NubankClone/NubankClone/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NubankClone.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace NubankClone
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new InitialPage();
         }
 
         protected override void OnStart()
